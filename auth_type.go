7cb74e8d45e228b228abd897844d7411d1f5caec
llq007 <code@llq007.cn>
2019-10-23 17:46:48 +0800

    fix bug

--- auth_type.go
+++ auth_type.go
@@ -180,6 +180,6 @@ type UserAuthInfoRsp struct {
 		IsCertified 		string `json:"is_certified"`// 是否通过实名认证。T是通过 F是没有实名认证。
 		UserType 			string `json:"user_type"`	// 用户类型（1/2） 1代表公司账户2代表个人账户
 		UserStatus			string `json:"user_status"`	//用户状态（Q/T/B/W）。 Q代表快速注册用户 T代表已认证用户 B代表被冻结账户 W代表已注册，未激活的账户
-	} `json:"alipay_system_oauth_token_response"`
+	} `json:"alipay_user_info_share_response"`
 	Sign string `json:"sign"`
 }
